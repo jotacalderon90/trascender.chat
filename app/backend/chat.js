98349270e97b8d6f7619861f3b050638b90b040a
jhonatan <jcalderon.aprog@gmail.com>
2020-05-02 19:54:31 -0300

    0.0.1

---
+++ app/backend/chat.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const fs = require("fs");
+const io = require("socket.io");
+
+let self = function(a){
+	this.render = a.render;
+	this.dir = a.dir;
+	this.config = a.config;
+	this.server = io(a.server);
+	this.sockets = {};
+	this.users_info = {};
+	this.server.on("connection", (socket)=>{this.connection(socket)});
+}
+
+
+
+//@route('/')
+//@method(['get'])
+self.prototype.render_index = function(req,res,next){
+	res.render("index");
+}
+
+self.prototype.connection = function(socket){
+	let ip = socket.request.connection.remoteAddress;
+	this.sockets[ip] = socket;
+	this.sockets[ip].on("mts", (data)=>{this.mts(data)});
+	this.sockets[ip].on("disconnect", ()=>{this.disconnect(ip)});
+}
+
+self.prototype.mts = function(data){
+	let d = {};
+	try{
+		d.nickname = data.nickname;
+		d.thumb = data.thumb;
+		d.msg = data.msg;
+		d.time = new Date();
+	}catch(e){
+		console.error("socket:mts:" + e.toString());
+		console.error(e);
+		d.error = e.toString();
+	}
+	this.server.sockets.emit("mtc", d);
+}
+
+self.prototype.disconnect = function(ip){
+	console.log(ip + " disconnect");
+}
+
+
+
+module.exports = self;
--- app/frontend/js/chat.js
+++ app/frontend/js/chat.js
@@ -1,30 +1,15 @@
-app.controller("chatCtrl", function(trascender,trascender_user,$scope){
+app.controller("chatCtrl", function(trascender,$scope){
 	
 	var self = this;
 	
-	this.user = trascender_user;
-	
 	this.chat = new trascender({
-		start: async function(){
-			await self.user.checkUser();
-			
-			if(self.user.doc==null && ["empty","expired"].indexOf(self.user.readLog.xhttp.json.error)>-1){
-				return;
+		start: function(){
+			this.user = {
+				nickname: "anonymous",
+				password: "secret",
+				thumb: "/media/img/logo.png"
 			}
-			
-			this.showChat = localStorage.getItem("showChat");
-			
-			if(document.getElementsByTagName("title")[0].innerHTML.trim()=="Chat"){
-				this.toggle(0);
-				$("#dvchat").fadeOut();
-			}
-			
 			this.message = "";
-			this.coll = [];
-			this.coll2 = [];
-			this.users = {};
-			this.list = $("#chat");
-			
 			if(host.indexOf("localhost")>-1){
 				this.socket = io({transports: ['websocket']});
 			}else{
@@ -32,61 +17,51 @@ app.controller("chatCtrl", function(trascender,trascender_user,$scope){
 				let o = (h.indexOf("https://")>-1)?{secure:true}:{};
 				this.socket = io(h,o);
 			}
-			
-			this.socket.on("first_load", (data)=>{this.first_load(data)});
-			this.socket.on("new_user", (data)=>{this.new_user(data)});
-			this.socket.on("delete_user", (data)=>{this.delete_user(data)});
 			this.socket.on("mtc", (data)=>{this.receive(data)});
-			
-		},
-		toggle: function(i){
-			this.showChat = i;
-			localStorage.setItem("showChat", this.showChat);
 		},
 		keypress: function(event){
 			if(event.originalEvent.which == 13) {
-				this.append();
-			}
-		},
-		first_load: function(data){
-			this.id = data.id;
-			this.users = data.users;
-			this.socket.emit("shared_profile", {
-				id: this.id,
-				nickname: self.user.doc.nickname,
-				thumb: self.user.doc.thumb
-			});
-			if(data.last_msg){
-				for(let i=0;i<data.last_msg.length;i++){
-					this.receive(data.last_msg[i]);
+				this.message = this.message.trim();
+				if(this.message!=""){
+					this.socket.emit("mts",{
+						nickname: this.user.nickname,
+						thumb: this.user.thumb,
+						msg: $.jCryption.encrypt(btoa(this.message),this.user.password)
+					});
+					this.message = "";
 				}
 			}
-			$scope.$digest(function(){});
 		},
-		receive: function(data){						
-			data.message = $.jCryption.decrypt(data.message, "secret");
-			data.message = atob(data.message);
-			this.coll.push(data);
-			this.coll2 = this.coll.reverse();
-			$scope.$digest(function(){});
-			this.list.animate({ scrollTop: this.list.prop("scrollHeight")}, 200);
-			//document.getElementById("blop").play();
+		receive: function(data){
+			console.log(data);
+			data.msgb= data.msg;
+			data.msg = $.jCryption.decrypt(data.msg, this.user.password);
+			data.msg = atob(data.msg);
+			this.pushMSG(data);
+			document.getElementById("blop").play();
 		},
-		new_user: function(data){
-			this.users[data.id] = data;
-			$scope.$digest(function(){});
-		},
-		delete_user: function(data){
-			delete this.users[data];
-			$scope.$digest(function(){});
-		},
-		append: function(){
-			this.message = this.message.trim();
-			if(this.message!=""){
-				this.socket.emit("mts",$.jCryption.encrypt(btoa(this.message),"secret"));
-				this.message = "";
-				this.toggle(1);
-			}
+		pushMSG: function(data){
+			this.n = "";
+			this.n += '<li class="list-group-item">';
+			this.n += '<img src="{{row.thumb}}" title="{{row.nickname}}"/>';
+			this.n += '<p>{{row.msg}}. <small>{{row.time}}</small></p>';
+			this.n += '</li>';
+			
+			let c = document.getElementById("chat");
+			
+			let li = document.createElement("li");
+			li.setAttribute("class","list-group-item");
+			let img = document.createElement("img");
+			img.src = data.thumb;
+			img.title = data.nickname;
+			let p = document.createElement("p");
+			p.innerHTML = ((data.msg!="")?data.msg:data.msgb);
+			let s = document.createElement("small");
+			s.innerHTML =  moment(new Date(data.time)).format("dddd, MMMM Do YYYY, h:mm:ss a");
+			p.appendChild(s);
+			li.appendChild(img);
+			li.appendChild(p);
+			c.insertBefore(li, c.firstChild);
 		}
 	});	
 	
